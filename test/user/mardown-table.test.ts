27e6261a477599fd5180d8ab7bfaa2778cef76b6
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-09 19:05:09 +0200

    🧪 Additional user tests.

--- test/user/mardown-table.test.ts
+++ test/user/mardown-table.test.ts
@@ -12,4 +12,48 @@ describe('markdownTable', () => {
       expect(actual).toBe(expected);
     });
   });
+
+  describe('when given a header and a row with values', () => {
+    it('returns a markdown table with the values.', () => {
+      const actual = markdownTable(['a', 'b'], ['1', '2']);
+      const expected = [
+        '| a | b |',
+        '| - | - |',
+        '| 1 | 2 |',
+      ].join('\n');
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('when given a header and multiple rows with values', () => {
+    it('returns a markdown table with the values.', () => {
+      const actual = markdownTable(['a', 'b'], ['1', '2'], ['3', '4']);
+      const expected = [
+        '| a | b |',
+        '| - | - |',
+        '| 1 | 2 |',
+        '| 3 | 4 |',
+      ].join('\n');
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('when given a lot of columns with varying length values', () => {
+    it('returns a markdown table with the values.', () => {
+      const actual = markdownTable(
+        ['a', 'b', 'c', 'd'],
+        ['1', '2', '3', '4'],
+        ['5', '6', '7', '8'],
+        ['9', '10', '11', '12'],
+      );
+      const expected = [
+        '| a | b  | c  | d  |',
+        '| - | -- | -- | -- |',
+        '| 1 | 2  | 3  | 4  |',
+        '| 5 | 6  | 7  | 8  |',
+        '| 9 | 10 | 11 | 12 |',
+      ].join('\n');
+      expect(actual).toBe(expected);
+    });
+  });
 });
