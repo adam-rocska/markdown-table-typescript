f2a670bc397ff3e79ce7c058ab5104d4db3f4e76
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-08-09 19:21:04 +0200

    🧪 Some more basic janky unit tests.

---
+++ test/unit/greatest-length-per-column.test.ts
@@ -0,0 +1,67 @@
+import {greatestLengthPerColumn} from '!src/greatest-length-per-column';
+
+describe('greatestLengthPerColumn', () => {
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+
+  it('returns the greatest length of each column in the given rows.', () => {
+    const lengths = greatestLengthPerColumn(
+      ['a', 'bb', 'ccc'],
+      ['dddd', 'ee', 'f']
+    );
+    expect(lengths).toEqual([4, 2, 3]);
+  });
+});
